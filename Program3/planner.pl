1aa0d7b94648df85def415c904221f486926bad2
Munchlaxx <evan.mirschel@gmail.com>
2019-03-19 16:06:50 -0400

    Added logic

--- Program3/planner.pl
+++ Program3/planner.pl
@@ -12,7 +12,7 @@
 :- module( planner,
 	   [
 	       plan/4,change_state/3,conditions_met/2,member_state/2,
-	       move/3,go/2,test/0,test2/0
+	       move/3,go/2,test1/0,test2/0
 	   ]).
 
 :- [utils].
@@ -41,29 +41,43 @@ member_state(S, [_|T]) :-	member_state(S, T).
 
 /* move types */
 
-move(pickup(X), [handempty, clear(X), on(X, Y)],
-		[del(handempty), del(clear(X)), del(on(X, Y)),
-				 add(clear(Y)),	add(holding(X))]).
+move(pickup(X), 
+	[handempty, clear(X, R), on(X, Y, R), armroom(R)],
+	[del(handempty), del(clear(X, R)), del(on(X, Y, R)),
+		add(clear(Y, R)),	add(holding(X))]).
 
-move(pickup(X), [handempty, clear(X), ontable(X)],
-		[del(handempty), del(clear(X)), del(ontable(X)),
-				 add(holding(X))]).
+move(pickup(X), 
+	[handempty, clear(X, R), ontable(X, R), armroom(R)],
+	[del(handempty), del(clear(X, R)), del(ontable(X, R)),
+		add(holding(X))]).
 
-move(putdown(X), [holding(X)],
-		[del(holding(X)), add(ontable(X)), add(clear(X)),
-				  add(handempty)]).
+move(putdown(X), 
+	[holding(X), armroom(R)],
+	[del(holding(X)), add(ontable(X, R)), add(clear(X, R)),
+		add(handempty)]).
 
-move(stack(X, Y), [holding(X), clear(Y)],
-		[del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)),
-				  add(clear(X))]).
+move(stack(X, Y), 
+	[holding(X), clear(Y, R), armroom(R)],
+	[del(holding(X)), del(clear(Y, R)), add(handempty), add(on(X, Y, R)),
+		add(clear(X, R))]).
+
+/* Added movement commands to go to room1 and room2 */
+move(goroom1,
+	[armroom(2)],
+	[del(armroom(2)), add(armroom(1))]).
+
+move(goroom2,
+	[armroom(1)],
+	[del(armroom(1)), add(armroom(2))]).
+
 /* run commands */
 
 go(S, G) :- plan(S, G, [S], []).
 
-test :- go([handempty, ontable(b), ontable(c), on(a, b), clear(c), clear(a)],
-	          [handempty, ontable(c), on(a,b), on(b, c), clear(a)]).
+test1 :- go([handempty, ontable(b, 1), ontable(c, 1), on(a, b, 1), clear(c, 1), clear(a, 1), armroom(1)],
+	          [handempty, ontable(c, 1), on(a, b, 1), on(b, c, 1), clear(a, 1), armroom(1)]).
 
-test2 :- go([handempty, ontable(b), ontable(c), on(a, b), clear(c), clear(a)],
-	          [handempty, ontable(a), ontable(b), on(c, b), clear(a), clear(c)]).
+test2 :- go([handempty, ontable(b, 1), ontable(c, 1), on(a, b, 1), clear(c, 1), clear(a, 1), armroom(1)],
+	          [handempty, ontable(b, 2), on(c, b, 2), on(a, c, 2), clear(a, 2), armroom(1)]).
 
 
